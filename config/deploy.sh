cf84330071dba9d84e9409a5df139b748d439bc5
kotarominami <kotaroisme@gmail.com>
2024-11-02 11:53:25 +0700

    Setup CICd Server Deploy #1

--- config/deploy.sh
+++ config/deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-version=$1 # x.y.z
+version=$1
 owner=$2
 token=$3
 actor=$4
 
 echo $token | docker login ghcr.io -u $actor --password-stdin
+echo -e "$actor $token" >> token.txt
 make -C web/ deploy TAG=$version OWNER=$owner
