5f7ba28f9d26a7a7d2cb0fdcd7b21547bddbe342
kotarominami <kotaroisme@gmail.com>
2024-11-02 09:31:31 +0700

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<container class="flex h-screen items-center justify-center bg-orange-500">
+	<div class="font-bold text-white">
+		Welcome to event<span class=" text-[2rem]">"Deployment [FE]"</span><br />
+	</div>
+</container>
