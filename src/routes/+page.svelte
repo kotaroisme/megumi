46ce22caf45f1f19f6accfe0bb9126f5f4d1b2f1
kotarominami <kotaroisme@gmail.com>
2024-11-02 12:17:46 +0700

    Hello world

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <container class="flex h-screen items-center justify-center bg-orange-500">
 	<div class="font-bold text-white">
 		Welcome to event<span class=" text-[2rem]">"Deployment [FE]"</span><br />
+		Success <span class=" text-[1rem]">Deploy</span>
 	</div>
 </container>
